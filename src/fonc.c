25bc13a7c9c8b3ffa82f65be58c702d36dfe6785
SaltyMold <168920293+SaltyMold@users.noreply.github.com>
2025-03-28 17:34:50 +0100

    Update fonc.c

--- src/fonc.c
+++ src/fonc.c
@@ -1,15 +1,7 @@
 #include "eadk.h"
-#include <assert.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 /*---------------------------------------------------------------------------*/
 
-// Random int between min and max
 int randint(int min, int max) {
     if (min > max) {
         int temp = min;
@@ -22,7 +14,6 @@ int randint(int min, int max) {
 
 /*---------------------------------------------------------------------------*/
 
-// Random choice in list
 const char* choice(const char *list[], size_t size) {
     int index = randint(0, size - 1);
     return list[index];
@@ -30,7 +21,6 @@ const char* choice(const char *list[], size_t size) {
 
 /*---------------------------------------------------------------------------*/
 
-// Instant crash and reset of the calculator
 void crash_calculator() {
     volatile int *ptr = (int *)0xFFFFFFFF;
     *ptr = 0;
@@ -38,16 +28,12 @@ void crash_calculator() {
 
 /*---------------------------------------------------------------------------*/
 
-// All function for the input : 
-
-// Mapping of keys to characters
 typedef struct {
     eadk_key_t key;
     char normal;
     char alpha;
 } KeyMapping;
 
-// Mapping of keys to characters
 static const KeyMapping key_mappings[] = {
     {eadk_key_one, '1', 'w'},
     {eadk_key_two, '2', 'x'},
@@ -80,23 +66,31 @@ static const KeyMapping key_mappings[] = {
     {eadk_key_tangent, 0, 'i'}
 };
 
-// refresh the screen with the buffer and the alpha mode for input()
-void refresh_input(const char *buffer, eadk_point_t point, bool is_alpha, bool draw_string, bool large_font) {
+void refresh_input(const char *buffer, size_t index, eadk_point_t point, bool is_alpha, bool draw_string, bool large_font) {
     if (draw_string) {
-        eadk_display_push_rect_uniform((eadk_rect_t){0, 0, EADK_SCREEN_WIDTH, EADK_SCREEN_HEIGHT}, eadk_color_white);
+        
+        if (large_font){
+            eadk_display_push_rect_uniform((eadk_rect_t){point.x, point.y, 10 + 10 * index, 18}, eadk_color_white);
+        }
+        else{
+            eadk_display_push_rect_uniform((eadk_rect_t){point.x, point.y, 7 * index, 14}, eadk_color_white);
+        }
         eadk_display_draw_string(buffer, point, large_font, eadk_color_black, eadk_color_white);
-        eadk_display_draw_string("Press home to quit", (eadk_point_t){180, 220}, false, eadk_color_black, eadk_color_white);
+        eadk_display_draw_string("Press exe to finish", (eadk_point_t){0, 200}, false, eadk_color_black, eadk_color_white);
+        eadk_display_draw_string("Press back to quit", (eadk_point_t){0, 220}, false, eadk_color_black, eadk_color_white);
+
         if (is_alpha) {
-            eadk_display_draw_string("ALPHA", (eadk_point_t){0, 0}, large_font, eadk_color_black, eadk_color_white);
+            eadk_display_push_rect_uniform((eadk_rect_t){240, 0, 80, 18}, eadk_color_white);
+            eadk_display_draw_string("ALPHA", (eadk_point_t){270, 0}, large_font, eadk_color_black, eadk_color_white);
         }
         else {
-            eadk_display_draw_string("NO ALPHA", (eadk_point_t){0, 0}, large_font, eadk_color_black, eadk_color_white);
+            eadk_display_push_rect_uniform((eadk_rect_t){240, 0, 80, 18}, eadk_color_white);
+            eadk_display_draw_string("NO ALPHA", (eadk_point_t){240, 0}, large_font, eadk_color_black, eadk_color_white);
         }
     }
     eadk_timing_msleep(300);
 }
 
-// Return the input of the user with point, draw_string and large_font in argument
 char* input(eadk_point_t point, bool draw_string, bool large_font) {
     static char buffer[100] = ""; 
     size_t index = 0;
@@ -105,7 +99,7 @@ char* input(eadk_point_t point, bool draw_string, bool large_font) {
     memset(buffer, 0, sizeof(buffer));
 
     if (draw_string) {
-        eadk_display_draw_string("NO ALPHA", (eadk_point_t){0, 0}, large_font, eadk_color_black, eadk_color_white);
+        eadk_display_draw_string("NO ALPHA", (eadk_point_t){240, 0}, large_font, eadk_color_black, eadk_color_white);
     }
 
     while (eadk_keyboard_scan() != 0) {
@@ -115,31 +109,31 @@ char* input(eadk_point_t point, bool draw_string, bool large_font) {
     while (true) {
         eadk_keyboard_state_t state = eadk_keyboard_scan();
 
-        if (eadk_keyboard_key_down(state, eadk_key_home)) {
-            return ""; // Exit input on 'HOME' key press
+        if (eadk_keyboard_key_down(state, eadk_key_back)) {
+            return ""; 
         } 
         else if (eadk_keyboard_key_down(state, eadk_key_backspace)) {
             if (index > 0) {
-                buffer[index - 1] = 0; // Remove the last character
+                buffer[index - 1] = 0;
                 index--;
-                refresh_input(buffer, point, is_alpha, draw_string, large_font);
+                refresh_input(buffer, index, point, is_alpha, draw_string, large_font);
             }
         } 
         else if (eadk_keyboard_key_down(state, eadk_key_alpha)) {
-            is_alpha = !is_alpha; // Toggle alpha mode on 'ALPHA' key press
-            refresh_input(buffer, point, is_alpha, draw_string, large_font);
+            is_alpha = !is_alpha; 
+            refresh_input(buffer, index, point, is_alpha, draw_string, large_font);
         }
         else if (eadk_keyboard_key_down(state, eadk_key_exe)) {
-            refresh_input(buffer, point, is_alpha, draw_string, large_font);
-            break; // Return the buffer on 'EXE' key press
+            refresh_input(buffer, index, point, is_alpha, draw_string, large_font);
+            break; 
         } 
         else {
             for (size_t i = 0; i < sizeof(key_mappings) / sizeof(KeyMapping); i++) {
                 if (eadk_keyboard_key_down(state, key_mappings[i].key)) {
                     char character = is_alpha ? key_mappings[i].alpha : key_mappings[i].normal;
-                    if (character != 0) { // Only add character if it's not 0
+                    if (character != 0) { 
                         buffer[index++] = character;
-                        refresh_input(buffer, point, is_alpha, draw_string, large_font);
+                        refresh_input(buffer, index, point, is_alpha, draw_string, large_font);
                     }
                     break;
                 }
@@ -151,16 +145,21 @@ char* input(eadk_point_t point, bool draw_string, bool large_font) {
 
 /*---------------------------------------------------------------------------*/
 
-void refresh_input_int(const char *buffer, eadk_point_t point, bool draw_string, bool large_font) {
+void refresh_input_int(const char *buffer, size_t index, eadk_point_t point, bool draw_string, bool large_font) {
     if (draw_string) {
-        eadk_display_push_rect_uniform((eadk_rect_t){0, 0, EADK_SCREEN_WIDTH, EADK_SCREEN_HEIGHT}, eadk_color_white);
+        if (large_font){
+            eadk_display_push_rect_uniform((eadk_rect_t){point.x, point.y, 10 + 10 * index, 18}, eadk_color_white);
+        }
+        else{
+            eadk_display_push_rect_uniform((eadk_rect_t){point.x, point.y, 7 * index, 14}, eadk_color_white);
+        }
         eadk_display_draw_string(buffer, point, large_font, eadk_color_black, eadk_color_white);
-        eadk_display_draw_string("Press home to quit", (eadk_point_t){180, 220}, false, eadk_color_black, eadk_color_white);
+        eadk_display_draw_string("Press exe to finish", (eadk_point_t){0, 200}, false, eadk_color_black, eadk_color_white);
+        eadk_display_draw_string("Press back to quit", (eadk_point_t){0, 220}, false, eadk_color_black, eadk_color_white);
     }
     eadk_timing_msleep(300);
 }
 
-// Mapping of keys to characters
 static const KeyMapping key_mappings_int[] = {
     {eadk_key_one, '1'},
     {eadk_key_two, '2'},
@@ -174,7 +173,6 @@ static const KeyMapping key_mappings_int[] = {
     {eadk_key_zero, '0'}
 };
 
-// Return the int input of the user with point, draw_string and large_font in argument
 int input_int(eadk_point_t point, bool draw_string, bool large_font) {
     static char buffer[100] = ""; 
     size_t index = 0;
@@ -188,34 +186,34 @@ int input_int(eadk_point_t point, bool draw_string, bool large_font) {
     while (true) {
         eadk_keyboard_state_t state = eadk_keyboard_scan();
 
-        if (eadk_keyboard_key_down(state, eadk_key_home)) {
-            return 0; // Exit input on 'HOME' key press
+        if (eadk_keyboard_key_down(state, eadk_key_back)) {
+            return 0; 
         } 
         else if (eadk_keyboard_key_down(state, eadk_key_backspace)) {
             if (index > 0) {
-                buffer[index - 1] = 0; // Remove the last character
+                buffer[index - 1] = 0; 
                 index--;
-                refresh_input_int(buffer, point, draw_string, large_font);
+                refresh_input_int(buffer, index, point, draw_string, large_font);
             }
         } 
         else if (eadk_keyboard_key_down(state, eadk_key_exe)) {
-            refresh_input_int(buffer, point, draw_string, large_font);
-            break; // Return the buffer on 'EXE' key press
+            refresh_input_int(buffer, index, point, draw_string, large_font);
+            break; 
         } 
         else {
             for (size_t i = 0; i < sizeof(key_mappings_int) / sizeof(KeyMapping); i++) {
                 if (eadk_keyboard_key_down(state, key_mappings_int[i].key)) {
                     char character = key_mappings_int[i].normal;
-                    if (character != 0) { // Only add character if it's not 0
+                    if (character != 0) { 
                         buffer[index++] = character;
-                        refresh_input_int(buffer, point, draw_string, large_font);
+                        refresh_input_int(buffer, index, point, draw_string, large_font);
                     }
                     break;
                 }
             }
         }
     }
-    return atoi(buffer); // Convert the buffer to an integer and return it
+    return atoi(buffer); 
 }
 
 /*---------------------------------------------------------------------------*/
@@ -277,14 +275,13 @@ eadk_color_t palette[] = {
     EADK_COLOR(196, 198, 184)
 };
 
-// Copy this code in the main.c file
+// Copy this code to your c file
 /*
 const char *image_code[] = {
     "", // Add your image code 
 };
 */
 
-// Decode and draw an image
 //ex : decode_and_draw_image((eadk_rect_t){0, 0, EADK_SCREEN_WIDTH, EADK_SCREEN_HEIGHT}, image_code[0]); 
 void decode_and_draw_image(eadk_rect_t rect, const char *code) {
     int x = rect.x, y = rect.y;
@@ -323,17 +320,16 @@ void chrono(eadk_point_t point, bool large_font, int duration) {
     int minutes = duration / 60;
     int seconds = duration % 60;
     int tenths = 0;
-    char buffer[8]; // Buffer to hold the time string in "MM:SS.T" format
+    char buffer[8];
 
     for (int i = duration * 10; i >= 0; i--) {
         snprintf(buffer, sizeof(buffer), "%02d:%02d.%d", minutes, seconds, tenths);
         eadk_display_draw_string(buffer, point, large_font, eadk_color_black, eadk_color_white);
-        eadk_timing_msleep(100); // Wait for 100 milliseconds
+        eadk_timing_msleep(100); 
 
-        // Check if the home key is pressed
         eadk_keyboard_state_t state = eadk_keyboard_scan();
         if (eadk_keyboard_key_down(state, eadk_key_home)) {
-            return; // Exit the function if home key is pressed
+            return; 
         }
 
         if (tenths == 0) {
@@ -355,70 +351,43 @@ void chrono(eadk_point_t point, bool large_font, int duration) {
 
 /*---------------------------------------------------------------------------*/
 
-// Factorial function
-unsigned long long factorial(unsigned int n) {
-    if (n == 0) {
-        return 1;
+void secure_sleep(int duration) {
+    int iterations = duration / 200;
+    for (int i = 0; i < iterations; i++) {
+        eadk_timing_msleep(200);
+        
+        eadk_keyboard_state_t state = eadk_keyboard_scan();
+        if (eadk_keyboard_key_down(state, eadk_key_home) || eadk_keyboard_key_down(state, eadk_key_back) || eadk_keyboard_key_down(state, eadk_key_on_off)) {
+            break;
+        }
     }
-    unsigned long long result = 1;
-    for (unsigned int i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
 }
 
 /*---------------------------------------------------------------------------*/
 
-// Return true if n is a prime number
-bool is_prime(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
+//ex: display_a_bool(var, "var", (eadk_point_t){0, 0});
+//output: var = false
+void display_a_bool(bool a, const char* var_name, eadk_point_t point) {
+    char buffer[30];
+    snprintf(buffer, sizeof(buffer), "%s = %s", var_name, a ? "true" : "false");
+
+    eadk_display_draw_string(buffer, point, false, eadk_color_black, eadk_color_white);
 }
 
 /*---------------------------------------------------------------------------*/
 
-// Function to decompose a number into its prime factors and return a string
-char* prime_factors(int n) {
-    static char result[256]; // Fixed size for the result string
-    char buffer[20]; // Buffer to convert numbers to strings
-    result[0] = '\0'; // Initialize the result string
+//ex: display_an_int(var, "var", (eadk_point_t){0, 0});
+//output: var = 5
+void display_an_int(int a, const char* var_name, eadk_point_t point) {
+    char buffer[30];
+    snprintf(buffer, sizeof(buffer), "%s = %d", var_name, a);
 
-    // Divide by 2 until n is odd
-    while (n % 2 == 0) {
-        sprintf(buffer, "%d*", 2);
-        strcat(result, buffer);
-        n /= 2;
-    }
+    eadk_display_draw_string(buffer, point, false, eadk_color_white, eadk_color_black);
+}
 
-    // n must be odd at this point, so we can skip even numbers
-    for (int i = 3; i * i <= n; i += 2) {
-        // While i divides n, add i and divide n
-        while (n % i == 0) {
-            sprintf(buffer, "%d*", i);
-            strcat(result, buffer);
-            n /= i;
-        }
-    }
+/*---------------------------------------------------------------------------*/
 
-    // This condition is for the case when n is a prime number
-    // greater than 2
-    if (n > 2) {
-        sprintf(buffer, "%d", n);
-        strcat(result, buffer);
-    } else {
-        // Remove the last '*' if present
-        size_t len = strlen(result);
-        if (len > 0 && result[len - 1] == '*') {
-            result[len - 1] = '\0';
-        }
-    }
-
-    return result;
+bool when_any_key_pressed() {
+    eadk_keyboard_state_t state = eadk_keyboard_scan();
+    return state != 0;
 }
